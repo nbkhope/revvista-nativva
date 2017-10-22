4fdd8f61056d2cc84e854e578f392a5f82dc4385
nbkhope <ryumemaru@yahoo.com>
2017-10-21 22:47:07 -0700

    Commit inicial

---
+++ App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { View, StyleSheet } from 'react-native';
+
+class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#c19a6b'
+  },
+});
+
+export default App;
