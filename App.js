f978cf18e487d4c56e012426d28d5bd124f8f47d
nbkhope <ryumemaru@yahoo.com>
2017-10-21 23:04:49 -0700

    Adicionar vistas interiores

--- App.js
+++ App.js
@@ -5,7 +5,28 @@ class App extends Component {
   render() {
     return (
       <View style={styles.container}>
+        <View style={{ flex: 1, flexDirection: 'row' }}>
+          <View style={{ flex: 1, backgroundColor: '#832a0d' }}>
+          </View>
+          <View style={{ flex: 1, backgroundColor: '#832a0d' }}>
+          </View>
+          <View style={{ flex: 1, backgroundColor: '#832a0d' }}>
+          </View>
+          <View style={{ flex: 1, backgroundColor: '#832a0d' }}>
+          </View>
+        </View>
 
+        <View style={{ flex: 1 }}>
+        </View>
+
+        <View style={{ flex: 1 }}>
+        </View>
+
+        <View style={{ flex: 1 }}>
+        </View>
+
+        <View style={{ flex: 1 }}>
+        </View>
       </View>
     );
   }
@@ -14,7 +35,8 @@ class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#c19a6b'
+    backgroundColor: '#c19a6b',
+    padding: 13
   },
 });
 
